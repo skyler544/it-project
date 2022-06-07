5c8673e01cf70b20e5c620c80a4822bc25ddcccf
Skyler Mayfield <skyler544@gmail.com>
2022-06-07 21:56:03 +0200

    fixed size canvas for movement testing
    TODO: add canvas bounding

--- js/anim-tester.js
+++ js/anim-tester.js
@@ -2,53 +2,74 @@
 document.addEventListener("DOMContentLoaded", function () {
   let canvas = document.getElementById("animTest");
   let context = canvas.getContext("2d");
+  canvas.setAttribute('width', 600);
+  canvas.setAttribute('height', 600);
 
   let spriteSheetPath = "asset/mystic_woods/characters/player.png";
   let spriteSheet = new Image();
   spriteSheet.src = spriteSheetPath;
-  spriteSheet.crossOrigin = false;
 
   let spriteW, spriteH, sheetW, sheetH;
   let colCount = 6;
-  let attackCount = 4;
-  let moveCount = colCount;
   let rowCount = 5;
   let animFrame = 0;
-  let idle = 0;
-  let walk = 1;
-  let attack = 2;
-  let die = 4;
+  let isMoving = 0;
+  let moveAmount = 10;
+  let offsetX = 0;
+  let offsetY = 0;
+  let rightPressed = false;
+  let leftPressed = false;
+  let upPressed = false;
+  let downPressed = false;
 
   let setVars = function () {
     sheetW = spriteSheet.width;
     sheetH = spriteSheet.height;
     spriteW = sheetW / colCount;
     spriteH = sheetH / rowCount;
-    // canvas.setAttribute('width', sheetW);
-    // canvas.setAttribute('height', sheetH);
-    canvas.setAttribute('width', spriteW * 2);
-    canvas.setAttribute('height', spriteH * 2);
+    console.log(sheetW);
+    console.log(sheetH);
+    console.log(spriteW);
+    console.log(spriteH);
   }
 
   spriteSheet.onload = function () {
     setVars();
-    // let interval = setInterval(animate, 250, idle);
-    let interval = setInterval(animate, 100, moveCount, walk);
-    // // let interval = setInterval(animate, 100, attackCount, attack);
-    // let interval = setInterval(animate, 100, attackCount, die);
+    console.log(sheetW);
+    console.log(sheetH);
+    console.log(spriteW);
+    console.log(spriteH);
+    let moveInterval = setInterval(animate, 60);
   }
 
-  let animate = function (frameCount, sequence) {
-    let pos = spritePosToImgPos(animFrame % frameCount, sequence);
-
+  let animate = function () {
+    if (upPressed || downPressed || rightPressed || leftPressed) {
+      isMoving = 1;
+    } else {
+      isMoving = 0;
+    }
+    movePlayer();
+    let pos = spritePosToImgPos(animFrame % colCount, isMoving);
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     context.drawImage(spriteSheet,
       pos.x, pos.y,
       spriteW, spriteH,
-      0, 0,
+      offsetX, offsetY,
       spriteW * 2, spriteH * 2);
     animFrame++;
+  }
+
+  let movePlayer = function () {
+    if (rightPressed) {
+      offsetX += moveAmount;
+    } else if (leftPressed) {
+      offsetX -= moveAmount;
+    } else if (upPressed) {
+      offsetY -= moveAmount;
+    } else if (downPressed) {
+      offsetY += moveAmount;
+    }
   }
 
   let spritePosToImgPos = function (col, row) {
@@ -58,16 +79,30 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  let drawEntireSheet = function () {
-    for (let i = 0; i < colCount; i++) {
-      for (let j = 0; j < rowCount; j++) {
-        let pos = spritePosToImgPos(i, j);
-        context.drawImage(spriteSheet,
-          pos.x, pos.y,
-          spriteW, spriteH,
-          pos.x, pos.y,
-          spriteW, spriteH);
-      }
+  let keyDownHandler = function (e) {
+    if (e.key == "d" || e.key == "Right" || e.key == "ArrowRight") {
+      rightPressed = true;
+    } else if (e.key == "a" || e.key == "Left" || e.key == "ArrowLeft") {
+      leftPressed = true;
+    } else if (e.key == "w" || e.key == "Up" || e.key == "ArrowUp") {
+      upPressed = true;
+    } else if (e.key == "s" || e.key == "Down" || e.key == "ArrowDown") {
+      downPressed = true;
     }
   }
+
+  let keyUpHandler = function (e) {
+    if (e.key == "d" || e.key == "Right" || e.key == "ArrowRight") {
+      rightPressed = false;
+    } else if (e.key == "a" || e.key == "Left" || e.key == "ArrowLeft") {
+      leftPressed = false;
+    } else if (e.key == "w" || e.key == "Up" || e.key == "ArrowUp") {
+      upPressed = false;
+    } else if (e.key == "s" || e.key == "Down" || e.key == "ArrowDown") {
+      downPressed = false;
+    }
+  }
+
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
 });
