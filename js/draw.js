0a6d41400b764c43057ee0841caa4b2743550084
Max2github <Your@email>
2022-07-01 11:50:26 +0200

    a little bit of refactoring

--- js/draw.js
+++ js/draw.js
@@ -5,19 +5,19 @@
 var gameArea = {
     // context,
     // canvas : { width, height },
-    load : function () {
+    load: function () {
         // not especially needed, you execute it after the page is loaded -> e.g. $(document).ready(gameArea.load)
         gameArea.canvas = document.getElementById("game");
         gameArea.context = gameArea.canvas.getContext("2d");
         gameArea.resize();
     },
     // please also check if in vertical mode - or force vertical mode ...
-    resize : function () { // absolute size not important! -> only proportion
+    resize: function () { // absolute size not important! -> only proportion
         // resizing only dependent from height - don't make it also dependent from the width, beacause then the proportion will be lost!!!
         gameArea.canvas.height = window.innerHeight * 3;  // * 3 for better resolution
         gameArea.canvas.width = gameArea.canvas.height * 1.535 // 1.535 times as wide as the height - can / should be changed
-    }, 
-    clear : function() {
+    },
+    clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
